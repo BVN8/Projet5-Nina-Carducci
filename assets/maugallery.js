79dff6786457fbd9a6c415a7d5bda1237f404664
Benjamin Vincent-Neveu <b.vincent.neveu@gmail.com>
2024-04-25 15:59:33 +0200

    Retirement du defer qui empêchait la mise en place de la modale au clic + changement mineurs sur le css

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,7 +153,7 @@
           index = i ;
         }
       });
-      next =  imagesCollection[index-1] || imagesCollection[imagesCollection.length - 1]
+      preview = imagesCollection[--index] || imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
@@ -190,7 +190,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index+1] || imagesCollection[0];
+      next = imagesCollection[++index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
